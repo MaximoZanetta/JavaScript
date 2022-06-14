b1b42cdd1f803e0fefd8f5a855a2a9d5a8a33544
maximozanetta <maximozanetta@gmail.com>
2022-06-14 20:28:59 -0300

    agregando eventos

--- jugar.js
+++ jugar.js
@@ -1,15 +1,40 @@
 
-function sinRegistrarse(){
-    let nombre = prompt("Ingrese su nombre de usuario");
-    
+// function sinRegistrarse(){
+//     const userName = document.getElementsByTagName("label").value;
 
-    if(nombre == "" ){
-        sinRegistrarse();
-    }
-    else {
-    const user = document.getElementById("userName");
-    user.innerHTML = `<strong>user: </strong>${nombre}`
-    }
-}
+//     if(userName == "" ){
+//         sinRegistrarse();
+//     }
+//     else {
+//     const user = document.getElementById("userName");
+//     user.innerHTML = `<strong>user: </strong>${nombre}`
+//     }
+// }
 
-sinRegistrarse()
+
+// sinRegistrarse();
+
+      
+          
+
+        
+
+const abrir = document.querySelector(".modal-btn");
+const modal = document.querySelector(".modal");
+const cerrar = document.querySelector(".modal-cerrar");
+
+abrir.addEventListener("click", (e)=>{
+    e.preventDefault();
+    modal.classList.add("modal-show");
+})
+
+cerrar.addEventListener("click", (e)=>{
+    e.preventDefault();
+    modal.classList.remove("modal-show");
+})
+
+
+const usuario = document.getElementsByClassName("input-name").value;
+          const user = document.getElementById("userName");
+          user.innerHTML = `<strong>user: </strong>${usuario}`;
+          console.log(usuario);
